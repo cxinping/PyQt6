65c12ca41731282e1ae9bcf6ade57a09555a6506
cxinping <>
2021-08-13 11:54:18 +0800

    PySide6中 QTreeWidget 例子

---
+++ Chapter05/qt_treewidget01.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+'''
+    【简介】
+	PySide6中 QTreeWidget 例子
+   
+  
+'''
+
+import sys
+from PySide6.QtWidgets import *
+from PySide6.QtGui import QIcon, QBrush, QColor
+from PySide6.QtCore import Qt
+
+
+class TreeWidgetDemo(QMainWindow):
+    def __init__(self, parent=None):
+        super(TreeWidgetDemo, self).__init__(parent)
+        self.setWindowTitle('TreeWidget 例子')
+        self.tree = QTreeWidget()
+        # 设置列数
+        self.tree.setColumnCount(2)
+        # 设置头的标题
+        self.tree.setHeaderLabels(['Key', 'Value'])
+        # 设置根节点
+        root = QTreeWidgetItem(self.tree)
+        root.setText(0, 'root')
+        root.setIcon(0, QIcon("./images/root.png"))
+        # 设置列宽
+        self.tree.setColumnWidth(0, 160)
+
+        ### 设置节点的背景颜色
+        # brush_red = QBrush(Qt.red)
+        # root.setBackground(0, brush_red)
+        # brush_green = QBrush(Qt.green)
+        # root.setBackground(1, brush_green)
+
+        # 设置子节点1
+        child1 = QTreeWidgetItem(root)
+        child1.setText(0, 'child1')
+        child1.setText(1, 'ios')
+        child1.setIcon(0, QIcon("./images/IOS.png"))
+        child1.setCheckState(0, Qt.Checked)
+
+        # 设置子节点2
+        child2 = QTreeWidgetItem(root)
+        child2.setText(0, 'child2')
+        child2.setText(1, '')
+        child2.setIcon(0, QIcon("./images/android.png"))
+
+        # 设置子节点3
+        child3 = QTreeWidgetItem(child2)
+        child3.setText(0, 'child3')
+        child3.setText(1, 'android')
+        child3.setIcon(0, QIcon("./images/music.png"))
+
+        self.tree.addTopLevelItem(root)
+        # 结点全部展开
+        self.tree.expandAll()
+
+        self.setCentralWidget(self.tree)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    tree = TreeWidgetDemo()
+    tree.show()
+    sys.exit(app.exec())
---
+++ Chapter05/qt_treewidget02.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+'''
+    【简介】
+	PySide6中 QTreeWidget 例子
+   
+  
+'''
+
+from PySide6.QtWidgets import *
+import sys
+
+
+class TreeWidgetDemo(QMainWindow):
+    def __init__(self, parent=None):
+        super(TreeWidgetDemo, self).__init__(parent)
+        self.setWindowTitle('TreeWidget 例子')
+        self.tree = QTreeWidget()
+        # 设置列数
+        self.tree.setColumnCount(2)
+        # 设置头的标题
+        self.tree.setHeaderLabels(['Key', 'Value'])
+        root = QTreeWidgetItem(self.tree)
+        root.setText(0, 'root')
+        root.setText(1, '0')
+
+        child1 = QTreeWidgetItem(root)
+        child1.setText(0, 'child1')
+        child1.setText(1, '1')
+
+        child2 = QTreeWidgetItem(root)
+        child2.setText(0, 'child2')
+        child2.setText(1, '2')
+
+        child3 = QTreeWidgetItem(root)
+        child3.setText(0, 'child3')
+        child3.setText(1, '3')
+
+        child4 = QTreeWidgetItem(child3)
+        child4.setText(0, 'child4')
+        child4.setText(1, '4')
+
+        child5 = QTreeWidgetItem(child3)
+        child5.setText(0, 'child5')
+        child5.setText(1, '5')
+
+        self.tree.addTopLevelItem(root)
+        self.tree.clicked.connect(self.onTreeClicked)
+
+        self.setCentralWidget(self.tree)
+
+    def onTreeClicked(self, qmodelindex):
+        item = self.tree.currentItem()
+        print("key=%s ,value=%s" % (item.text(0), item.text(1)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    tree = TreeWidgetDemo()
+    tree.show()
+    sys.exit(app.exec())
