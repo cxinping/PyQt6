0f7561dc73f4ddb7692230308b11d7d4b96ff1ba
cxinping <>
2021-08-13 09:22:38 +0800

    重构代码

---
+++ Chapter05/qt_tblViewModel.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*- 
+'''
+    【简介】
+	PySide6中QTableView表格视图控件的例子
+   
+  
+'''
+
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+import sys
+
+
+class Table(QWidget):
+
+    def __init__(self, arg=None):
+        super(Table, self).__init__(arg)
+        self.setWindowTitle("QTableView表格视图控件的例子")
+        self.resize(500, 300);
+        self.model = QStandardItemModel(4, 4);
+        self.model.setHorizontalHeaderLabels(['标题1', '标题2', '标题3', '标题4'])
+
+        for row in range(4):
+            for column in range(4):
+                item = QStandardItem("row %s, column %s" % (row, column))
+                self.model.setItem(row, column, item)
+
+        self.tableView = QTableView()
+        self.tableView.setModel(self.model)
+        # 下面代码让表格100填满窗口
+        # self.tableView.horizontalHeader().setStretchLastSection(True)
+        # self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        dlgLayout = QVBoxLayout();
+        dlgLayout.addWidget(self.tableView)
+        self.setLayout(dlgLayout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    table = Table()
+    table.show()
+    sys.exit(app.exec_())
