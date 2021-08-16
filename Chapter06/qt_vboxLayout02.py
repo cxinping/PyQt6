cb90e92791fd513455144a75f13c4582da61b019
cxinping <xpws2006@163.com>
2021-08-16 15:16:38 +0800

    单一的网格单元格

---
+++ Chapter06/qt_vboxLayout02.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+    单一的网格单元格
+
+"""
+
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton
+
+
+class Winform(QWidget):
+    def __init__(self, parent=None):
+        super(Winform, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        # 1
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        # 2
+        names = ['Cls', 'Back', '', 'Close',
+                 '7', '8', '9', '/',
+                 '4', '5', '6', '*',
+                 '1', '2', '3', '-',
+                 '0', '.', '=', '+']
+
+        # 3
+        positions = [(i, j) for i in range(5) for j in range(4)]
+
+        # 4
+        for position, name in zip(positions, names):
+            if name == '':
+                continue
+
+            button = QPushButton(name)
+            grid.addWidget(button, *position)
+
+        self.move(300, 150)
+        self.setWindowTitle('网格布局管理例子')
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Winform()
+    form.show()
+    sys.exit(app.exec())
