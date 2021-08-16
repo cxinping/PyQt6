eb33f3cfe5b10ae3ae8b7206747746baed1ccb93
cxinping <xpws2006@163.com>
2021-08-16 15:11:54 +0800

    水平布局管理例子

---
+++ Chapter06/qt_hboxLayout.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+    水平布局管理例子
+    
+    
+"""
+
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton
+
+
+class Winform(QWidget):
+    def __init__(self, parent=None):
+        super(Winform, self).__init__(parent)
+        self.setWindowTitle("水平布局管理例子")
+
+        # 水平布局按照从左到右的顺序进行添加按钮部件。
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(QPushButton(str(1)))
+        hlayout.addWidget(QPushButton(str(2)))
+        hlayout.addWidget(QPushButton(str(3)))
+        hlayout.addWidget(QPushButton(str(4)))
+        hlayout.addWidget(QPushButton(str(5)))
+        self.setLayout(hlayout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Winform()
+    form.show()
+    sys.exit(app.exec())
