b96962a07c8f82484d0e7a502e7f727548cda0c3
cxinping <xpws2006@163.com>
2021-08-16 15:13:53 +0800

    水平布局管理例子

---
+++ Chapter06/qt_layoutAddStretch01.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+	 布局中用到的addStretch函数例子
+
+"""
+
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+import sys
+
+
+class WindowDemo(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        btn1 = QPushButton(self)
+        btn2 = QPushButton(self)
+        btn3 = QPushButton(self)
+        btn1.setText('button 1')
+        btn2.setText('button 2')
+        btn3.setText('button 3')
+
+        hbox = QHBoxLayout()
+        # 设置伸缩量为1
+        hbox.addStretch(1)
+        hbox.addWidget(btn1)
+        # 设置伸缩量为1
+        hbox.addStretch(1)
+        hbox.addWidget(btn2)
+        # 设置伸缩量为1
+        hbox.addStretch(1)
+        hbox.addWidget(btn3)
+        # 设置伸缩量为1
+        hbox.addStretch(1)
+
+        self.setLayout(hbox)
+        self.setWindowTitle("addStretch 例子")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = WindowDemo()
+    win.show()
+    sys.exit(app.exec())
---
+++ Chapter06/qt_layoutAddStretch03.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+    水平布局管理例子
+        
+"""
+
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton
+
+
+class Winform(QWidget):
+    def __init__(self, parent=None):
+        super(Winform, self).__init__(parent)
+        self.setWindowTitle("水平布局管理例子")
+        self.resize(800, 50)
+
+        # 水平布局按照从左到右的顺序进行添加按钮部件。
+        hlayout = QHBoxLayout()
+
+        hlayout.addWidget(QPushButton(str(1)))
+        hlayout.addWidget(QPushButton(str(2)))
+        hlayout.addWidget(QPushButton(str(3)))
+        hlayout.addWidget(QPushButton(str(4)))
+        hlayout.addWidget(QPushButton(str(5)))
+        # 添加伸缩控件		
+        hlayout.addStretch(0)
+
+        # 设置间距
+        # hlayout.setSpacing( 10 )
+
+        self.setLayout(hlayout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Winform()
+    form.show()
+    sys.exit(app.exec())
