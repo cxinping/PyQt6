7428c2ba819123cd519ee228bd2334f36887843e
cxinping <xpws2006@163.com>
2021-08-16 15:21:32 +0800

    跨越行和列的网格单元格

---
+++ Chapter06/qt_vboxLayout03.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+    跨越行和列的网格单元格
+
+"""
+
+import sys
+from PySide6.QtWidgets import (QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication)
+
+class Winform(QWidget):
+    def __init__(self,parent=None):
+        super(Winform,self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        title = QLabel('标题')
+        author = QLabel('提交人')
+        review = QLabel('申告内容')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('故障申告')
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Winform()
+    form.show()
+    sys.exit(app.exec())
