62bd8a6f781f06f44a3e207452dff426589153b7
cxinping <xpws2006@163.com>
2021-10-03 09:06:50 +0800

    添加例子

---
+++ Chapter05/qt_tblView.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*- 
+"""
+    【简介】
+	PySide6中 QTableView表格视图控件的例子
+
+"""
+
+from PySide6.QtWidgets import (QTableView, QApplication, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QMessageBox,
+                               QAbstractItemView, QHeaderView)
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+import sys
+from PySide6.QtCore import Qt
+
+
+class Table(QWidget):
+
+    def __init__(self, arg=None):
+        super(Table, self).__init__(arg)
+        # 设置窗体的标题和初始大小
+        self.setWindowTitle("QTableView表格视图控件的例子")
+        self.resize(500, 300)
+
+        # 准备数据模型，设置数据层次结构为5行4列
+        self.model = QStandardItemModel(5, 4)
+        # 设置数据栏名称
+        self.model.setHorizontalHeaderLabels(['标题1', '标题2', '标题3', '标题4'])
+
+        for row in range(5):
+            for column in range(4):
+                item = QStandardItem("row %s, column %s" % (row, column))
+                self.model.setItem(row, column, item)
+
+        # 实例化表格视图，设置模型为自定义的模型
+        self.tableView = QTableView()
+        self.tableView.setModel(self.model)
+
+        ############ 下面代码让表格 100% 的填满窗口
+        self.tableView.horizontalHeader().setStretchLastSection(True)
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # 局部布局
+        vboxLayout = QVBoxLayout()
+        vboxLayout.addWidget(self.tableView)
+        # 全局布局
+        wl = QVBoxLayout(self)
+        wl.addLayout(vboxLayout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    table = Table()
+    table.show()
+    sys.exit(app.exec())
